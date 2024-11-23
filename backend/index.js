a6aad03b53d0c00925901eef8eb52ccac5ee0720
Ritik <ritikpatel82993@gmail.com>
2024-11-23 19:45:24 +0530

    frontend URL added to backend

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'https://job-portal-nine-tau.vercel.app/',
     credentials:true
 }
+
 
 app.use(cors(corsOptions));
 
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-
 import React, { useEffect } from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
