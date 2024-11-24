977dd0d6a72402b91965a55f387f590969881cff
Ritik <ritikpatel82993@gmail.com>
2024-11-24 11:52:32 +0530

    frontend URL added

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-iota-eight.vercel.app/',
+    origin:'http://localhost:5173',
   
     credentials:true
 }
