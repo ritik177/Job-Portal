a82d33a79e2f173d11fb3932f480c438383d29e4
Ritik <ritikpatel82993@gmail.com>
2024-11-25 08:53:35 +0530

    production ready code

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,14 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+//diployment
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.json());
@@ -26,7 +30,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+
 
 
 // api's
@@ -35,7 +39,11 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
+//deploy code
+app.use(express.static(path.join(_dirname,'frontend/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html")); 
+});///api/v1/user  in sab api ke baad jo bhi path aayega usko ye hit karega jaise- login/signup etc. 
 
 app.listen(PORT,()=>{
     connectDB();
